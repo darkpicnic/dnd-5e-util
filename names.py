d0088a4b3fbef5f8d67107b0780acd476df96977
Sebastian Lemery <sebastian@lemery.io>
2015-09-22 17:54:41 -0700

    Now loading data from YAML files

--- names.py
+++ names.py
@@ -1,52 +1,16 @@
+import yaml
 import random
 import constants
-
-HUMAN_MALE_FIRST_NAMES = (
-    "John", "William", "Garreth", "Jared"
-)
-
-HUMAN_FEMALE_FIRST_NAMES = (
-    "Agatha", "Elizabeth", "Jane", "Roberta"
-)
-
-HUMAN_LAST_NAMES = (
-    "Blackstone", "Crownsmith"
-)
-
-ELF_MALE_FIRST_NAMES = (
-    "Adran",
-    "Aelar",
-    "Aust",
-    "Beiro",
-    "Heian",
-)
-
-ELF_FEMALE_FIRST_NAMES = (
-    "Adrie",
-    "Althaea",
-    "Anastrianna",
-    "Drusilia",
-    "Lia",
-)
-
-ELF_LAST_NAMES = (
-    "Amakiir",
-    "Amastacia",
-    "Siannodel",
-)
 
 
 def generate_name(race, gender):
     """ Create a first and last name for character
     """
     normalized_race = race.lower()
-    if normalized_race == constants.ELF:
-        first = random.choice(ELF_MALE_FIRST_NAMES) if gender == constants.MALE \
-        else random.choice(ELF_FEMALE_FIRST_NAMES)
-        last = random.choice(ELF_LAST_NAMES)
-    elif normalized_race == constants.HUMAN:
-        # TODO Finish
-        first = random.choice(HUMAN_MALE_FIRST_NAMES) if gender == constants.MALE \
-        else random.choice(HUMAN_FEMALE_FIRST_NAMES)
-        last = random.choice(HUMAN_LAST_NAMES)
+    stream = file('data/names/{}.yaml'.format(normalized_race), 'r')
+    data = yaml.load(stream)
+
+    first = random.choice(data[gender.lower() + '_first_names'])
+    last = random.choice(data['last_names'])
+
     return first, last
